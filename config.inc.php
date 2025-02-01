45e94c3df745b4fbe83942a7be47f0ca14253c27
NATTHAWAT <natthawat.derb@gmail.com>
2025-02-01 09:20:28 +0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,86 +1,21 @@
 <?php
-/**
- * phpMyAdmin sample configuration, you can use it as base for
- * manual configuration. For easier setup you can use setup/
- *
- * All directives are explained in documentation in the doc/ folder
- * or at <https://docs.phpmyadmin.net/>.
- */
-
 declare(strict_types=1);
 
-/**
- * This is needed for cookie based authentication to encrypt the cookie.
- * Needs to be a 32-bytes long string of random bytes. See FAQ 2.10.
- */
-$cfg['blowfish_secret'] = 'qtdRoGmbc9{8IZr323xYcSN]0s)r$9b_'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'qtdRoGmbc9{8IZr323xYcSN]0s)r$9b_'; // ห้ามแก้ไขค่านี้
 
-/**
- * Servers configuration
- */
 $i = 0;
-
-/**
- * First server
- */
-
-/**
- * phpMyAdmin configuration storage settings.
- */
-
-/* User used to manipulate with storage */
-// $cfg['Servers'][$i]['controlhost'] = '';
-// $cfg['Servers'][$i]['controlport'] = '';
-// $cfg['Servers'][$i]['controluser'] = 'pma';
-// $cfg['Servers'][$i]['controlpass'] = 'pmapass';
-
-/* Storage database and tables */
-// $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-// $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
-// $cfg['Servers'][$i]['relation'] = 'pma__relation';
-// $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
-// $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
-// $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
-// $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
-// $cfg['Servers'][$i]['history'] = 'pma__history';
-// $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
-// $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
-// $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
-// $cfg['Servers'][$i]['recent'] = 'pma__recent';
-// $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
-// $cfg['Servers'][$i]['users'] = 'pma__users';
-// $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
-// $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
-// $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
-// $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
-// $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
-// $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
-/* Contrib / Swekey authentication */
-// $cfg['Servers'][$i]['auth_swekey_config'] = '/etc/swekey-pma.conf';
-
-/**
- * Second server
- */
 $i++;
-/* Authentication type */
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['verbose'] = 'MariaDB 10';
-/* Server parameters */
-$cfg['Servers'][$i]['socket'] = '/var/run/mariadb10.sock';
-$cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
 
-
-
-$cfg['Servers'][1]['host'] = '192.168.100.10';
-$cfg['Servers'][1]['port'] = '3306';
-$cfg['Servers'][1]['user'] = 'yuanlin';
-$cfg['Servers'][1]['password'] = 'yuanli@888';
-$cfg['Servers'][1]['auth_type'] = 'config';
-
-/**
- * End of servers configuration
- */
+// การเชื่อมต่อ TCP/IP (สำหรับ MariaDB บน 192.168.100.10)
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['verbose'] = 'MariaDB 10'; // หรือเวอร์ชั่นที่ถูกต้อง
+$cfg['Servers'][$i]['host'] = '192.168.100.10';
+$cfg['Servers'][$i]['port'] = '3306';
+$cfg['Servers'][$i]['user'] = 'yuanlin';
+$cfg['Servers'][$i]['password'] = 'yuanli@888';
+$cfg['Servers'][$i]['compress'] = false; // เพิ่มบรรทัดนี้
+$cfg['Servers'][$i]['AllowNoPassword'] = false; // เพิ่มบรรทัดนี้
+ 
 
 /**
  * Directories for saving/loading files from server
